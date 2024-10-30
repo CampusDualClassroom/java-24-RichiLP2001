5c7c380e8b17eb64d1fcf79a41f5c6ec46757449
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-30 18:11:58 +0100

    Java, ejercicio 24. TEST completado

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,13 +5,28 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> nuevaCola = new LinkedList<>();
+        nuevaCola.offer("Smith");
+        nuevaCola.offer("Montessori");
+        nuevaCola.offer("Peralta");
+        nuevaCola.offer("House");
+        return nuevaCola;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
 
     }
 
     public static void main(String[] args) {
+        Queue<String> laCola = createQueue();
+        printAndEmptyQueue(laCola);
+
+        System.out.println(laCola.peek()+"\n");
+        System.out.println("\n siguiente prueba: ");
+
+
 
     }
 
